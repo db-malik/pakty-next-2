5bbe165ff686511e35bddb916134057d64bb134a
db-malik <dbmalek92@gmail.com>
2023-04-08 09:36:57 +0100

    add : inspiration page

--- src/containers/inspirationPage/InspirationPage.js
+++ src/containers/inspirationPage/InspirationPage.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Filter from '../../components/filter/Filter'
-import SliderInspiration from '../../components/fragments/inspirationFragments/sliderInspiration/SliderInspiration'
 import Collection from '../../components/fragments/landingFragments/collection/Collection'
+import Slider from '../../components/fragments/comonFragment/slider/Slider'
 
 import { ButtonsInspirationFilter } from '/data/filterData'
 
 const InspirationPage = () => {
   return (
     <div>
-      <SliderInspiration />
-      <Filter buttons={ButtonsInspirationFilter} />
+      <Slider
+        title="Decor ideas for your space"
+        image="/assets/img/slider-2.jpg"
+        content="If you're searching for the newest trends or modest ideas for 3D Furniture: we've got you the best."
+      />
+
+      <Filter
+        title="Choose the type of the room :"
+        buttons={ButtonsInspirationFilter}
+      />
       <Collection title="" subtitle="" numberCardGallery={3} />
     </div>
   )
