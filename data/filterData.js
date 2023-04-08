35164043a263fb5b4e7feb930a18bb6197a298ba
db-malik <dbmalek92@gmail.com>
2023-04-08 09:36:25 +0100

    add data to mock fetched data from API

--- data/filterData.js
+++ data/filterData.js
@@ -30,3 +30,36 @@ export const ButtonsInspirationFilter = [
     action: '',
   },
 ]
+
+export const ButtonsStoreFilter = [
+  {
+    id: 0,
+    label: 'All products',
+    iconSrc: '',
+    action: '',
+  },
+  {
+    id: 1,
+    label: 'Living room',
+    iconSrc: '/assets/icons/living_room.png',
+    action: '',
+  },
+  {
+    id: 2,
+    label: 'Bedroom',
+    iconSrc: '/assets/icons/bedroom.png',
+    action: '',
+  },
+  {
+    id: 3,
+    label: 'Kitchen',
+    iconSrc: '/assets/icons/kitchen.png',
+    action: '',
+  },
+  {
+    id: 4,
+    label: 'Dinning room',
+    iconSrc: '/assets/icons/table.png',
+    action: '',
+  },
+]
---
+++ data/oneProductDetail.js
@@ -0,0 +1,37 @@
+export const productDetail = {
+  _id: '63c04fec2c7d2ce19d34feed',
+  entitled: 'Salon Kenza',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis elementum massa. Pellentesque sollicitudin gravida sagittis. Etiam at metus lacus. Nulla facilisi. Etiam sit amet libero nulla. Mauris pulvinar fermentum magna, pulvinar suscipit tortor facilisis non. Nullam sit amet lorem vel tellus congue tempus id non leo. Nullam porta nisl vel condimentum viverra.',
+  price: 3500,
+  materials: ['Bois MDF', 'Plastique', 'Textile'],
+  measurements: {
+    height: 1,
+    width: 3.5,
+    depth: 1,
+    weight: 150,
+    _id: '63c04fec2c7d2ce19d34feee',
+  },
+  images: [
+    '/assets/img/product/Img-2.jpg',
+    '/assets/img/product/Img-3.jpg',
+    '/assets/img/product/Img-4.jpg',
+    '/assets/img/product/Img-5.jpg',
+  ],
+  colors: ['#FF5733', '#E4D44E', '#FFFFFF', '#000000', '#2BC3CD', '#D68D1B'],
+  textures: [
+    'http://pm.basketofart.net:3000/static/photo-1673547549944-454281166.png',
+    'http://pm.basketofart.net:3000/static/photo-1673547567148-199155676.png',
+    'http://pm.basketofart.net:3000/static/photo-1673547578426-267407419.png',
+    'http://pm.basketofart.net:3000/static/photo-1673547608318-532725691.png',
+  ],
+  bundle3d: 'url',
+  category: '63c04b252c7d2ce19d34feda',
+  maxProdPerWeek: 23,
+  owner: '63bbee3d2c7d2ce19d34fc34',
+  addedBy: '63b846f22c7d2ce19d34fb4b',
+  status: 'Available',
+  createdAt: '2023-01-12T18:22:36.783Z',
+  __v: 0,
+  updatedAt: '2023-01-29T15:30:47.789Z',
+}
